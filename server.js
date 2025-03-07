452234a4d1c18e293160cf47dcdcce46a2cc5b26
nihatsaydam <nihat.saydam@icloud.com>
2025-03-08 01:24:12 +0300

    önemli update
    sa

--- server.js
+++ server.js
@@ -372,6 +372,6 @@ app.get('/', (req, res) => {
 
 // Sunucuyu başlat
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
