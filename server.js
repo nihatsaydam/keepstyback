fca5babe26dbcaa33479a02ad33926fe4f16660b
nihatsaydam <nihat.saydam@icloud.com>
2025-03-08 04:53:30 +0300

    yanlış update attım az once canımm
    düzelttim

--- server.js
+++ server.js
@@ -445,13 +445,14 @@ app.get('/getRoomservices', async (req, res) => {
   }
 });
 
+
 // Ana sayfa endpoint'i (Opsiyonel)
 app.get('/', (req, res) => {
   res.send('Welcome to Keepsty Backend API!');
 });
 
 // Sunucuyu başlat
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
